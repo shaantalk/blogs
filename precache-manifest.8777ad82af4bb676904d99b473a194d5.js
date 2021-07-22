4edd43d8cddbd9c0b49c2906b4c50d49a5e281f2
Santanu Panda <shaantalk@gmail.com>
2021-07-23 04:05:32 +0530

    Updates

---
+++ precache-manifest.8777ad82af4bb676904d99b473a194d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f09afa4daaf105dfe26a42c2ededdf8",
+    "url": "/blogs/index.html"
+  },
+  {
+    "revision": "4c57ffb4ec5194c46415",
+    "url": "/blogs/static/css/main.1b920327.chunk.css"
+  },
+  {
+    "revision": "4186e25e86c08a852864",
+    "url": "/blogs/static/js/2.bb29e5a2.chunk.js"
+  },
+  {
+    "revision": "c7bba65e859bd8a2b8fa18d202b74500",
+    "url": "/blogs/static/js/2.bb29e5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c57ffb4ec5194c46415",
+    "url": "/blogs/static/js/main.603cc41e.chunk.js"
+  },
+  {
+    "revision": "cdb06c7e58e05070dd06",
+    "url": "/blogs/static/js/runtime-main.87f2a3dd.js"
+  }
+]);
